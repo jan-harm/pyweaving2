05335331d1606bd12ec7c480c78c95b3aff3c228
jan-harm <jan.harm@deboerenwolters.nl>
2024-07-31 18:15:35 +0200

    uodate to stc struct, removed nose, six and py27 (AND REMOVED SOME TODOS)

--- src/pyweaving2/render.py
+++ src/pyweaving2/render.py
@@ -4,8 +4,6 @@ from __future__ import (absolute_import, division, print_function,
 import os.path
 
 from PIL import Image, ImageDraw, ImageFont
-# todo: replace PIL with pillow
-
 
 __here__ = os.path.dirname(__file__)
 
--- src/pyweaving2/wif.py
+++ src/pyweaving2/wif.py
@@ -2,7 +2,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from configparser import ConfigParser
-# todo: remove six (no 2.x support)
 
 from pyweaving2 import Draft, __version__
 
